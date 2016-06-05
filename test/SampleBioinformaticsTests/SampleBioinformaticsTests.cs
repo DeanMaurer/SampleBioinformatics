817c565c75bf3cadd5989a30d6b01b83250731ef
Dean Maurer <dean.a.maurer@gmail.com>
2016-06-04 18:56:00 -0700

    Added appveyor config. Added test project with one passing unit test using NUnit version 3

---
+++ test/SampleBioinformaticsTests/SampleBioinformaticsTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleBioinformaticsTests
+{
+    [TestFixture]
+    public class SampleBioinformaticsTests
+    {
+        [Test]
+        public void AlwaysPass()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
