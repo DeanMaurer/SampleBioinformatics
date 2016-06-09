eff576860f1cd639100a3b1da13e4ec4560da51c
Dean Maurer <dean.a.maurer@gmail.com>
2016-06-09 00:18:25 -0700

    Added the modernizr script back to the project because it's needed.

--- SampleBioinformatics/App_Start/BundleConfig.cs
+++ SampleBioinformatics/App_Start/BundleConfig.cs
@@ -23,6 +23,11 @@ namespace SampleBioinformatics
                 "~/app/config.js",
                 "~/app/index.js"));
 
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                "~/Scripts/modernizr-*"));
+
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                 "~/Scripts/bootstrap.js",
                 "~/Scripts/respond.js"));
