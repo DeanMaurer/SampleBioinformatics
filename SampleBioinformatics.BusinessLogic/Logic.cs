dd67ae06b06931ae52ef826ca3b68879a242d70b
Dean Maurer <dean.a.maurer@gmail.com>
2016-06-11 13:38:28 -0700

    Created a business logic and an interface project and updated a unit test to validate the results of a method in the business logic.

---
+++ SampleBioinformatics.BusinessLogic/Logic.cs
@@ -0,0 +1,17 @@
+﻿using SampleBioinformatics.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleBioinformatics.BusinessLogic
+{
+    public class Logic : ISampleBioinformatics
+    {
+        public string ReturnSuccess()
+        {
+            return "Success";
+        }
+    }
+}
---
+++ SampleBioinformatics.UnitTests/SampleBioinformaticsBusinessLogicTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using SampleBioinformatics.BusinessLogic;
+
+namespace SampleBioinformatics.UnitTests
+{
+    [TestFixture]
+    class SampleBioinformaticsBusinessLogicTests
+    {
+        Logic _sbLogic = new Logic();
+
+        [Test, Category("unit")]
+        public void CanReturnSuccess()
+        {
+            string success = _sbLogic.ReturnSuccess();
+            Assert.AreEqual("Success", success);
+        }
+    }
+}
