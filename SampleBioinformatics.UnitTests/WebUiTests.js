ad455504fa277f93ac0bd9aa0a82a0564706d0e4
Dean Maurer <dean.a.maurer@gmail.com>
2016-06-12 11:54:42 -0700

    Created a real javascript unit test that proves the index controller does the expected thing with the response that is expected to come from the web API.

--- SampleBioinformatics.UnitTests/WebUiTests.js
+++ SampleBioinformatics.UnitTests/WebUiTests.js
@@ -1,6 +1,37 @@
-﻿
+﻿/// <reference path="..\SampleBioinformatics.Web\Scripts\angular.js"/>
+/// <reference path="..\SampleBioinformatics.Web\Scripts\angular-ui-router.js"/>
+/// <reference path=".\Scripts\angular-mocks.js"/>
 
-test("Javascript Always Pass", function () {
-    equal(true, true);
+/// <reference path="..\SampleBioinformatics.Web\app\app.js"/>
+/// <reference path="..\SampleBioinformatics.Web\app\index.js"/>
+
+describe("UI", function () {
+
+    var index;
+
+    beforeEach(angular.mock.module("bio"));
+
+    beforeEach(inject(function ($controller) {
+        index = $controller("index");
+    }));
+
+    beforeEach(inject(function ($injector) {
+        $httpBackend = $injector.get("$httpBackend");
+        requestHandler = $httpBackend.when("GET", "/api/SampleBioinformatics/GetDNADecoded?DNA=AAA")
+    }));
+
+    afterEach(function () {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it("should handle API repsponse correctly", inject(function () {
+        requestHandler.respond(200, "{\"DNA\":\"AAA\",\"mRNA\":\"UUU\",\"tRNA\":\"AAA\",\"AminoAcids\":[\"Lysine\"]}");
+
+        index.dna = "AAA";
+        index.decodeDNA();
+        $httpBackend.flush();
+        expect(index.decodedInfo).toEqual(Object({ DNA: 'AAA', mRNA: 'UUU', tRNA: 'AAA', AminoAcids: ['Lysine'] }));
+
+    }));
 });
-
